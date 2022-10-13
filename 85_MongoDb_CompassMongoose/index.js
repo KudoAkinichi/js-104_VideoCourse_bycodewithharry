f7e9368efadac073ef5a34efc7c6d71648f19618
KudoAkinichi <arookrishna@gmail.com>
2022-10-13 23:20:58 +0530

    Tutorial 85 | Half | Mongoose & MongoDb

--- 85_MongoDb_CompassMongoose/index.js
+++ 85_MongoDb_CompassMongoose/index.js
@@ -1,3 +1,4 @@
+// npm install mongoose
 // getting-started.js
 const mongoose = require('mongoose');
 
@@ -9,3 +10,22 @@ async function main() {
   
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
+
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+// // NOTE: methods must be added to the schema before compiling it with mongoose.model()
+// kittySchema.methods.speak = function speak() {
+//   const greeting = "My name is " + this.name
+//   console.log(greeting);
+// };
+
+const Kitten = mongoose.model('kudoKitten', kittySchema);
+
+const kudoKitty = new Kitten({ name: 'kudoKitty pro' });
+// console.log(kudoKitty.name); 
+// kudoKitty.speak()
+
+
+
